259e199e2a05f5b500880d2e966845304fb2976c
aliciaparisse <alicia.parisse@gmail.com>
2014-08-20 23:44:41 +0200

    Formattage
    Writting a function that takes the description text and transforms it
    into html code with tags. Using [b][/b] for bold for example.
    And adding tags kind of everywhere in the descriptions.

--- js/events.js
+++ js/events.js
@@ -2,133 +2,160 @@
 var width = Math.min($(window).width(),1024);
 
 // creates multi-line text
-function _createText(text) {
-    var lines = text.split('\n');
-    var parag = document.createElement('p');
-    for(var i=0, line; line = lines[i]; i++) {
-      if(i>0) {
-        parag.appendChild(document.createElement('br'));
-      }
-      parag.appendChild(document.createTextNode(line));
-    }
-    return parag;
+function _parseText(text) {
+	var parag = "";
+	var continuer = true;
+	for (var i = 0; i<text.length; i++)
+	{
+		continuer = true;
+		var j;
+		if (text.charAt(i)=="[")
+		{
+			console.log(text.substring(0,5) + i+1 + continuer);
+			//console.log("This is i:"+text.charAt(i));
+			for (j=i+1; (j<(text.length)) && (continuer == true); j++)
+			{
+				console.log("This is j:" + text.charAt(j))
+				if (text.charAt(j)=="]")
+				{
+					if (text.charAt(i+1)!="/")
+					{
+						parag+="<"+text.substring((i+1),j)+">";
+					}
+					else
+					{
+						console.log("I'm even HERE !!" + text.substring((i+1),j));
+						parag+="<"+text.substring((i+1),j)+">";		
+					}
+					continuer = false
+				}
+			}
+			i+=(j-i)-1;
+		}
+		else 
+		{
+			parag+=text.charAt(i);
+		}
+	}
+	return parag;
 }
 
 function getStringDate(date)
 {
-    var options = {weekday: "long", day: "2-digit", month: "long", hour: "2-digit", minute:"2-digit"};
-    var str = date.toLocaleString("fr-FR",options);
-    //First letter of the words to upper case
-    str = str.replace(/([a-z])([a-z]*)/g, function(match,p1,p2){return p1.toUpperCase() + p2;}); 
-    return str;
+	var options = {weekday: "long", day: "2-digit", month: "long", hour: "2-digit", minute:"2-digit"};
+	var str = date.toLocaleString("fr-FR",options);
+	//First letter of the words to upper case
+	str = str.replace(/([a-z])([a-z]*)/g, function(match,p1,p2){return p1.toUpperCase() + p2;}); 
+	return str;
 }
 
 $.getJSON('events.json',function(data) {
-    console.log(data); // this will show the info it in firebug console 
-    var events = data.events;
-    var mainDiv = document.getElementById("events");
-    
-    var grid = document.createElement("ul");
-    grid.className = "small-block-grid-2 medium-block-grid-3 large-block-grid-4";
-    mainDiv.appendChild(grid);
-    for( var i = 0; i < events.length; ++i)
-    {
-        var modalName = "modalEvent"+i;
-        var evt = events[i];
-        var bloc = document.createElement("li");
-        bloc.className = "text-center event-thumbnail";
-        
-        var link = document.createElement("a");
-        link.href = "#";
-        //link.className = "th [radius]";
-        link.setAttribute("data-reveal-id",modalName);
-        
-        var img = document.createElement("img");
+	console.log(data); // this will show the info it in firebug console 
+	var events = data.events;
+	var mainDiv = document.getElementById("events");
+	
+	var grid = document.createElement("ul");
+	grid.className = "small-block-grid-2 medium-block-grid-3 large-block-grid-4";
+	mainDiv.appendChild(grid);
+	for( var i = 0; i < events.length; ++i)
+	{
+		var modalName = "modalEvent"+i;
+		var evt = events[i];
+		var bloc = document.createElement("li");
+		bloc.className = "text-center event-thumbnail";
+		
+		var link = document.createElement("a");
+		link.href = "#";
+		//link.className = "th [radius]";
+		link.setAttribute("data-reveal-id",modalName);
+		
+		var img = document.createElement("img");
 	img.src = "img/pirate_fille.png";
 		
-        img.alt = "image - "+evt.title;
-        
-        var title = document.createElement("h5");
-        title.className = "subheader";
-        title.textContent = evt.title;
-        
-        var date = document.createElement("h6");
-        
-        var now = new Date(2014,8,15);
-        var d = new Date(evt.d);
-        
-        var dateClass = "";
-        if( now.getDate() === d.getDate() ) // day from 1 to 31
-        {
-            dateClass = "soon";
-        } else if( now > d ) {
-            dateClass = "past";
-        } else {
-            dateClass = "future";
-        }
-        date.className = dateClass;
-        date.textContent = getStringDate(d);
-        
-        link.appendChild(img);
-        
-        bloc.appendChild(link);
-        bloc.appendChild(date);
-        bloc.appendChild(title);
-        
-        grid.appendChild(bloc);
-        
-        
-        // Creating reveal modal
-        var modal = document.createElement("div");
-        modal.id = modalName;
-        modal.className = "reveal-modal tiny";
-        modal.setAttribute("data-reveal","");
-        modal.innerHTML = "<a class=\"close-reveal-modal\">&#215;</a>";
-        
-        var title = document.createElement("h1");
-        title.textContent = evt.title;
-        
-        var gpslink = document.createElement("a");
-        gpslink.className = "th [radius]";
-        gpslink.href = "http://maps.google.com/maps?z=12&t=m&q="+evt.gps.replace(",","+");
-        gpslink.target = "_blank";
-        
-        var static_map = document.createElement("img");
-        static_map.alt = "Google Map - "+evt.title;
-        var url = "http://maps.googleapis.com/maps/api/staticmap?";
-        url += "size="+width+"x200";
-        url += "&markers=color:green|" + evt.gps;
-        static_map.src = "";
-        static_map.setAttribute("temp",url);
-        gpslink.appendChild(static_map);
-        
-        var content = _createText(evt.content);
-        var rdv = document.createElement("p");
-        rdv.appendChild(document.createTextNode(getStringDate(new Date(evt.d))));
-        rdv.appendChild(document.createElement("br"));
-        rdv.appendChild(document.createTextNode( "Lieu de rendez-vous : "+ evt.rdv ));
-        if( evt.tram > 0 ){
-            rdv.appendChild(document.createElement("br"));
-            rdv.appendChild(document.createTextNode("Tu auras besoin de "+evt.tram+" ticket"+(evt.tram > 1 ? "s":"")+" de tram.")) ;
-        }
-        modal.appendChild(title);
-        modal.appendChild(rdv);
-        
-        modal.appendChild(gpslink);
-        modal.appendChild(content);
-        
-        
-        mainDiv.appendChild(modal);
-    }
-    //console.log(events);
-    
-    $(document).foundation();
-    
-    $(document).on('open.fndtn.reveal', '[data-reveal]', function () {
-        // prevent all the Maps images to be loaded before we need them
-        var modal = $(this);
-        var img = modal.children("a").children("img")[0];
-        img.src = img.getAttribute("temp");
-    });
+		img.alt = "image - "+evt.title;
+		
+		var title = document.createElement("h5");
+		title.className = "subheader";
+		title.textContent = evt.title;
+		
+		var date = document.createElement("h6");
+		
+		var now = new Date(2014,8,15);
+		var d = new Date(evt.d);
+		
+		var dateClass = "";
+		if( now.getDate() === d.getDate() ) // day from 1 to 31
+		{
+			dateClass = "soon";
+		} else if( now > d ) {
+			dateClass = "past";
+		} else {
+			dateClass = "future";
+		}
+		date.className = dateClass;
+		date.textContent = getStringDate(d);
+		
+		link.appendChild(img);
+		
+		bloc.appendChild(link);
+		bloc.appendChild(date);
+		bloc.appendChild(title);
+		
+		grid.appendChild(bloc);
+		
+		
+		// Creating reveal modal
+		var modal = document.createElement("div");
+		modal.id = modalName;
+		modal.className = "reveal-modal tiny";
+		modal.setAttribute("data-reveal","");
+		modal.innerHTML = "<a class=\"close-reveal-modal\">&#215;</a>";
+		
+		var title = document.createElement("h1");
+		title.textContent = evt.title;
+		
+		var gpslink = document.createElement("a");
+		gpslink.className = "th [radius]";
+		gpslink.href = "http://maps.google.com/maps?z=12&t=m&q="+evt.gps.replace(",","+");
+		gpslink.target = "_blank";
+		
+		var static_map = document.createElement("img");
+		static_map.alt = "Google Map - "+evt.title;
+		var url = "http://maps.googleapis.com/maps/api/staticmap?";
+		url += "size="+width+"x200";
+		url += "&markers=color:green|" + evt.gps;
+		static_map.src = "";
+		static_map.setAttribute("temp",url);
+		gpslink.appendChild(static_map);
+		
+		var content = document.createElement('p');
+		content.innerHTML = _parseText(evt.content);
+		var rdv = document.createElement("p");
+		rdv.appendChild(document.createTextNode(getStringDate(new Date(evt.d))));
+		rdv.appendChild(document.createElement("br"));
+		rdv.appendChild(document.createTextNode( "Lieu de rendez-vous : "+ evt.rdv ));
+		if( evt.tram > 0 ){
+			rdv.appendChild(document.createElement("br"));
+			rdv.appendChild(document.createTextNode("Tu auras besoin de "+evt.tram+" ticket"+(evt.tram > 1 ? "s":"")+" de tram.")) ;
+		}
+		modal.appendChild(title);
+		modal.appendChild(rdv);
+		
+		modal.appendChild(gpslink);
+		modal.appendChild(content);
+		
+		
+		mainDiv.appendChild(modal);
+	}
+	//console.log(events);
+	
+	$(document).foundation();
+	
+	$(document).on('open.fndtn.reveal', '[data-reveal]', function () {
+		// prevent all the Maps images to be loaded before we need them
+		var modal = $(this);
+		var img = modal.children("a").children("img")[0];
+		img.src = img.getAttribute("temp");
+	});
 });
 
